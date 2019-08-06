e0e5b84d14c6d2a89a4f01205604f61479f381e6
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-06 18:22:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
-        :two_pieces_owned => 2,
+        :two_pieces_owned => 50,
         :three => 3,
         :four => 4
       },
